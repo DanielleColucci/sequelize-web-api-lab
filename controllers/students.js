d200c4fffe23f91b8ac7bbcd090d5a87aab3f5b0
DanielleColucci <ddcolucci99@gmail.com>
2023-02-15 15:30:23 -0500

    define create student route and controller function

--- controllers/students.js
+++ controllers/students.js
@@ -1,7 +1,14 @@
 const { Student } = require('../models')
 
-
+const create = async (req, res) => {
+  try {
+    const student = await Student.create(req.body)
+    res.status(200).json(student)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
 
 module.exports = {
-  
+  create,
 }
--- routes/students.js
+++ routes/students.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const studentsCtrl = require('../controllers/students.js')
 
-
-
+router.post('/', studentsCtrl.create)
 
 module.exports = router
